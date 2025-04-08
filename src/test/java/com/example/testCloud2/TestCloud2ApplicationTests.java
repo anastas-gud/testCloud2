5c89c8ffdef542f690acfcf0328391a8c6963ea6
Anastasia <nastya.gudoshnikova@list.ru>
2025-04-08 14:19:57 +0400

    Initial commit with Spring Boot project structure

---
+++ src/test/java/com/example/testCloud2/TestCloud2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testCloud2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCloud2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
